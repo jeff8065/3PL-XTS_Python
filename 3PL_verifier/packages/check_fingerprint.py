928da464a2d8674a601620663050ccee57d3ad3e
jeff8065 <37102860+jeff8065@users.noreply.github.com>
2020-10-23 18:15:56 +0800

    verifier

---
+++ 3PL_verifier/packages/check_fingerprint.py
@@ -0,0 +1,49 @@
+import subprocess, os, string, time, sys
+from typing import List
+
+
+def chenk_fingerprint(self):
+    a = sys.argv
+    a = (list(a))
+    print(len(a))
+    for i in a:
+        print(i)
+    #   print(" ")
+    # devicesID=sys.argv[1]
+    devicesID = []
+    text = []
+    text2 = []
+    k = 2
+    for i in ((a)):
+        # print(str(i) )
+        devicesID.append(i)
+        k = k + i
+    print(devicesID[2] + "hheheheheh")
+    for i in devicesID:
+        # print(i)
+        print("")
+    output1 = subprocess.check_output(("adb -s 872HADSR4DKRH shell getprop | grep fingerprint").split("\n"), shell=True)
+    output2 = subprocess.check_output(("adb -s 1895110299 shell getprop | grep fingerprint").split("\n"), shell=True)
+
+    for word in output1.split():
+        text.append(str(word))
+    # text2.append(str(word))
+
+    for word in output2.split():
+        # text.append(str(word))
+        text2.append(str(word))
+
+    text2.append("ohohohohohohohoh")
+
+    # print((str(text)) + "ttttttttttttttttttt")
+    # print((list(text)[0]) + "GGGGGGGGGGGGGGGGGGGG")
+    k = 0
+    s1 = set(text)
+    s2 = set(text2)
+    print(s1.symmetric_difference(s2))  # 字串比較
+    for i in text:
+        # print( str(k) + " " + (i) )
+        k = k + 1
+
+
+chenk_fingerprint(1)
